e28d9e724f29186ed9c6852156cb71ce7b2cbc77
artem <73790146+ArtemDeynega@users.noreply.github.com>
2022-04-26 18:14:49 +0300

    searchBar done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,17 +3,32 @@ import { Searchbar } from 'components/Searchbar';
 import { ImageGallery } from 'components/ImageGallery';
 import { Loader } from 'components/Loader';
 import { Modal } from 'components/Modal';
+import imagesApi from 'service/imageApi';
+import { toast, ToastContainer } from 'react-toastify';
 import { GlobalStyles } from 'Styles/GlobalStyles/GlobalStyles';
 
 export class App extends Component {
+  state = {
+    searchQuery: '',
+    page: 1,
+    status: 'indle',
+  };
+  handleNewQuery = newRequest => {
+    const { searchQuery } = this.props;
+    if (newRequest !== searchQuery) {
+      this.setState({ searchQuery: newRequest, page: 1, status: 'pending' });
+    }
+  };
   render() {
+    console.log(this.state.searchQuery);
     return (
       <>
-        <Searchbar />
-        <ImageGallery />
+        <Searchbar onSubmit={this.handleNewQuery} />
+        {/* <ImageGallery />
         <Loader />
-        <Modal />
+        <Modal /> */}
         <GlobalStyles />
+        <ToastContainer autoClose={3000} />
       </>
     );
   }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,7 +1,59 @@
 import { Component } from 'react';
+import { Header, Form, FormButton, ButtonLabel, FormIpnut } from '.';
+import { ImSearch } from 'react-icons/im';
+import { toast } from 'react-toastify';
+import PropTypes from 'prop-types';
 
 export class Searchbar extends Component {
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+  };
+  state = {
+    searchValue: '',
+  };
+  handleInputChange = evt => {
+    this.setState({ searchValue: evt.target.value });
+  };
+  handleSubmit = evt => {
+    evt.preventDefault();
+    const { searchValue } = this.state;
+    if (searchValue.trim() === '') {
+      toast.warn(`Введите слово для поиска 😕`);
+      return;
+    }
+
+    this.props.onSubmit(searchValue.trim().toLowerCase());
+    this.resetRequest();
+  };
+  resetRequest = () => {
+    this.setState({ searchValue: '' });
+  };
   render() {
-    return <div></div>;
+    const { searchValue } = this.state;
+    // console.log(searchValue);
+    return (
+      <>
+        <Header>
+          <Form onSubmit={this.handleSubmit}>
+            <FormButton type="submit">
+              <ImSearch
+                style={{
+                  marginRight: 'auto',
+                  marginLeft: 'auto',
+                  color: 'black',
+                }}
+              />
+              <ButtonLabel>Search</ButtonLabel>
+            </FormButton>
+            <FormIpnut
+              placeholder="Search images and photos"
+              value={searchValue}
+              onChange={this.handleInputChange}
+            />
+          </Form>
+        </Header>
+      </>
+    );
   }
 }
+// placeholder = 'Search';
